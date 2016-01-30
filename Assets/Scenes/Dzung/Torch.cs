47ac3d6115644d93c3c3195a1d16391059967f23
Dzung Nguyen <dzungnguyen17@gmail.com>
2016-01-30 11:37:46 -0800

    Add torch and interactable torch
    Created torch object and prefab
    Added collider and allows player to press R when in torch collider to
    turn torch on/off

---
+++ Assets/Scenes/Dzung/Torch.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Torch : MonoBehaviour {
+
+	private int torchValue = 1;
+	public Material matColor_lit;
+	public Material matColor_regular;
+	private TorchSequence ts;
+
+	private bool isLit;
+	public bool colliding;
+
+	public bool Light {
+		get {
+			return isLit;
+		}
+		set {
+			isLit = value;
+		}
+	}
+
+	void Start() {
+		ts = FindObjectOfType<TorchSequence> ();
+		colliding = false;
+	}
+
+	void Update() {
+		if (colliding) {
+			if (Input.GetKeyDown ("r")) {
+				if (isLit) {
+					isLit = false;
+					this.GetComponent<Renderer> ().material = matColor_regular;
+					ts.removeTorch (torchValue);
+				} else {
+					isLit = true;
+					this.GetComponent<Renderer> ().material = matColor_lit;
+					ts.addTorch (torchValue);
+				}
+			}
+		}
+
+	}
+
+	void OnTriggerEnter(Collider col) {
+		colliding = true;
+		Debug.Log ("colliding");
+	}
+
+	void OnTriggerExit(Collider col) {
+		colliding = false;
+		Debug.Log ("not colliding");
+	}
+
+}

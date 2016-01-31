7ce8179064811b878d217de5b97ddad2322b668c
vernw <vrnnwng@gmail.com>
2016-01-31 11:33:10 -0800

    Menu Implementation
    1/2

--- Assets/Scripts/MenuScripts/PressAnyKey.cs
+++ Assets/Scripts/MenuScripts/PressAnyKey.cs
@@ -2,15 +2,24 @@
 using DG.Tweening;
 using System.Collections;
 
-public class Pulse : MonoBehaviour
+public class PressAnyKey : MonoBehaviour
 {
-
     private TextMesh textMesh;
     private TextMesh titleTextMesh;
     public float faded;
     public float solid;
 
-    // Use this for initialization
+    public Camera menuCamera;
+
+    public Camera gameCamera;
+    public Vector3 gameCameraPos;
+    public Quaternion gameCameraRot;
+
+    void Awake()
+    {
+        gameCamera.enabled = false;
+    }
+    
     void Start()
     {
         textMesh = gameObject.GetComponent<TextMesh>();
@@ -18,11 +27,15 @@ public class Pulse : MonoBehaviour
         faded = 0.15f;
         solid = 1.0f;
 
+        gameCameraPos = gameCamera.transform.position;
+        gameCameraRot = gameCamera.transform.rotation;
+
         InvokeRepeating("FadeOut", 2.0f, 4.5f);
         InvokeRepeating("FadeIn", 4.0f, 4.5f);
         print(textMesh.color);
     }
 
+    // Pulse cycles
     public void FadeOut()
     {
         DOTween.ToAlpha(() => textMesh.color, x => textMesh.color = x, faded, 1);
@@ -35,18 +48,35 @@ public class Pulse : MonoBehaviour
 
     public void StartGame()
     {
-        // Camera pans to character; game begins; disable Title
+        // Cancels pulse animations
         CancelInvoke("FadeOut");
         CancelInvoke("FadeIn");
+
         // Fades text
         DOTween.ToAlpha(() => textMesh.color, x => textMesh.color = x, 0, 2.0f);
         DOTween.ToAlpha(() => titleTextMesh.color, x => titleTextMesh.color = x, 0, 2.0f);
-        Invoke("Disable", 2.0f);
+
+        // Moves and swaps camera
+        menuCamera.transform.DOMove(gameCameraPos, 2.0f);
+        menuCamera.transform.DORotate(gameCameraRot.eulerAngles, 2.0f);
+        Invoke("CameraSwap", 2.5f);
+
+        // Calls Disable() in 2.5 seconds
+        Invoke("Disable", 5.0f);
     }
 
+    // Swaps the menu camera to the game camera
+    public void CameraSwap()
+    {
+        Debug.Log("CameraSwap");
+        gameCamera.enabled = true;
+        menuCamera.enabled = false;
+    }
+
+    // Deactivates all menu assets
     public void Disable()
     {
-        GameObject.Find("Title").SetActive(false);
+        GameObject.FindGameObjectWithTag("Menu").SetActive(false);
     }
 
     void Update()
